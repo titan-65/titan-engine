7a2725366dc858b29a45c8c343836dfe63fdd096
titan-65 <teckcare2011@gmail.com>
2022-09-11 11:38:37 -0400

    created titan class

--- src/engine/index.js
+++ src/engine/index.js
@@ -1,3 +1,12 @@
-class TitanEngine {
+import { CALL_SIGN, LOSER_MODE, WINNER_MODE, PLAYER_VS_PLAYER } from '../types/constants'
+import { Player, Damage } from '../types/engine'
 
+class TitanEngine {
+    constructor(player: Player, damage: Damage, game) {
+        this.game = game
+        this.player = player
+        this.timer = {
+            specialMode: 0
+        }
+    }
 }
